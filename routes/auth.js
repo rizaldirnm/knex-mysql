dd29ea469e1d16b41fb1a3ea55fb30ce01928b44
Rizaldi Dev <rizaldinurm@gmail.com>
2019-04-06 21:00:33 +0700

    fix auth flow

--- routes/auth.js
+++ routes/auth.js
@@ -56,4 +56,8 @@ router.get("/current_user", Auth.isUserLogin, (req, res) => {
   res.json({ user: req.user });
 });
 
+// @route   GET /api/auth/logout
+// @desc    Show current user
+// @access  Private
+
 module.exports = router;
