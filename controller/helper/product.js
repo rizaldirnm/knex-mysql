776230cbd977464d3951cb73e22c546ec3d2336e
Rizaldi Dev <rizaldinurm@gmail.com>
2019-04-08 00:50:11 +0700

    delete product

--- controller/helper/product.js
+++ controller/helper/product.js
@@ -9,16 +9,30 @@ class ProductHelper {
           qty: data.qty,
           user_id: id
         })
-        .then(data => console.log(data))
-        .catch(() =>
-          reject({
-            errorType: DATA_NOT_POSTED,
-            message: "Terjadi kesalahan, data anda belum masuk"
-          })
+        .then(newProduct =>
+          resolve({ success: true, message: "Product successfull added" })
+        )
+        .catch(err =>
+          reject({ success: false, message: "Product fail to added" })
         );
+    });
+  }
 
-      const newProduct = await knex("products").orderBy("product_id", "desc");
-      resolve(newProduct[0]);
+  deleteProduct(id) {
+    return new Promise(async (resolve, reject) => {
+      const idProduct = await knex("products").where("product_id", id);
+      if (idProduct <= 0) {
+        return reject({ success: false, message: "Product not exist!" });
+      }
+      await knex("products")
+        .where("product_id", id)
+        .del()
+        .then(deleted =>
+          resolve({ success: true, message: "Product successfull delted" })
+        )
+        .catch(err =>
+          reject({ success: false, message: "Product fail to added" })
+        );
     });
   }
 }
--- routes/product.js
+++ routes/product.js
@@ -4,9 +4,11 @@ const Auth = require("../controller/middleware/auth");
 const helper = require("../controller/helper/product");
 const validateAddProduct = require("../controller/library/validation/addProduct");
 
+// ================================ ROUTER API BEGIN ================================
+
 // @route   POST api/product/add
 // @desc    Add Product
-// @access  Public
+// @access  Secret
 router.post("/add", Auth.isUserLogin, Auth.isAdmin, (req, res) => {
   const { errors, isValid } = validateAddProduct(req.body);
   if (!isValid) {
@@ -16,8 +18,24 @@ router.post("/add", Auth.isUserLogin, Auth.isAdmin, (req, res) => {
   const data = req.body;
   helper
     .addProduct(user_id, data)
-    .then(newProduct => res.json({ postdata: true, data: newProduct }))
+    .then(newProduct => res.json(newProduct))
     .catch(err => res.status(400).json({ postdata: false, error: err }));
 });
+
+// @route   POST api/product/delete
+// @desc    Delete product
+// @access  Secret
+router.delete(
+  "/delete/:id_product",
+  Auth.isUserLogin,
+  Auth.isAdmin,
+  (req, res) => {
+    const { id_product } = req.params;
+    helper
+      .deleteProduct(id_product)
+      .then(deletedProduct => res.json(deletedProduct))
+      .catch(err => res.status(400).json(err));
+  }
+);
 
 module.exports = router;
